72b5e2fc15a3f9eb9bd67098d8d1ca771445b832
elrafael <elrafael123@gmail.com>
2026-08-19 12:15:58 +0100

    feat: Initial Ionic project

--- src/app/folder/folder.page.ts
+++ src/app/folder/folder.page.ts
@@ -1,19 +1,67 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { IonHeader, IonToolbar, IonButtons, IonMenuButton, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { Camera } from '@capacitor/camera';
+import {
+  IonHeader,
+  IonToolbar,
+  IonButtons,
+  IonMenuButton,
+  IonTitle,
+  IonContent,
+  IonButton,
+  IonCard,
+  IonCardHeader,
+  IonCardTitle,
+  IonCardSubtitle,
+  IonCardContent,
+  IonImg,
+} from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-folder',
   templateUrl: './folder.page.html',
   styleUrls: ['./folder.page.scss'],
-  imports: [IonHeader, IonToolbar, IonButtons, IonMenuButton, IonTitle, IonContent],
+  imports: [
+    IonHeader,
+    IonToolbar,
+    IonButtons,
+    IonMenuButton,
+    IonTitle,
+    IonContent,
+    IonButton,
+    IonCard,
+    IonCardHeader,
+    IonCardTitle,
+    IonCardSubtitle,
+    IonCardContent,
+    IonImg,
+  ],
 })
 export class FolderPage implements OnInit {
   public folder!: string;
   private activatedRoute = inject(ActivatedRoute);
+  protected photoTaken = '';
   constructor() {}
 
   ngOnInit() {
     this.folder = this.activatedRoute.snapshot.paramMap.get('id') as string;
+  }
+
+  async takePhoto() {
+    try {
+      const result = await Camera.takePhoto({
+        quality: 90,
+        includeMetadata: true,
+      });
+
+      if (result.webPath) {
+        this.photoTaken = result.webPath;
+      }
+    } catch (error: any) {
+      const message = error.code
+        ? `[${error.code}] ${error.message}`
+        : error.message;
+      console.error('takePhoto failed:', message);
+    }
   }
 }
