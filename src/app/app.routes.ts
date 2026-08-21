789a48ba257bc1bc47655adce149f88353b351ad
elrafael <elrafael123@gmail.com>
2026-08-21 09:35:18 +0100

    refactor: clean up imports and remove unused photo functionality in folder page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -11,4 +11,12 @@ export const routes: Routes = [
     loadComponent: () =>
       import('./folder/folder.page').then((m) => m.FolderPage),
   },
+  {
+    path: 'camera',
+    loadComponent: () => import('./camera/camera.page').then( m => m.CameraPage)
+  },
+  {
+    path: 'weather',
+    loadComponent: () => import('./weather/weather.page').then( m => m.WeatherPage)
+  },
 ];
--- src/app/folder/folder.page.ts
+++ src/app/folder/folder.page.ts
@@ -1,20 +1,12 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { Camera } from '@capacitor/camera';
 import {
-  IonHeader,
-  IonToolbar,
   IonButtons,
+  IonContent,
+  IonHeader,
   IonMenuButton,
   IonTitle,
-  IonContent,
-  IonButton,
-  IonCard,
-  IonCardHeader,
-  IonCardTitle,
-  IonCardSubtitle,
-  IonCardContent,
-  IonImg,
+  IonToolbar,
 } from '@ionic/angular/standalone';
 
 @Component({
@@ -28,40 +20,14 @@ import {
     IonMenuButton,
     IonTitle,
     IonContent,
-    IonButton,
-    IonCard,
-    IonCardHeader,
-    IonCardTitle,
-    IonCardSubtitle,
-    IonCardContent,
-    IonImg,
   ],
 })
 export class FolderPage implements OnInit {
   public folder!: string;
   private activatedRoute = inject(ActivatedRoute);
-  protected photoTaken = '';
   constructor() {}
 
   ngOnInit() {
     this.folder = this.activatedRoute.snapshot.paramMap.get('id') as string;
-  }
-
-  async takePhoto() {
-    try {
-      const result = await Camera.takePhoto({
-        quality: 90,
-        includeMetadata: true,
-      });
-
-      if (result.webPath) {
-        this.photoTaken = result.webPath;
-      }
-    } catch (error: any) {
-      const message = error.code
-        ? `[${error.code}] ${error.message}`
-        : error.message;
-      console.error('takePhoto failed:', message);
-    }
   }
 }
