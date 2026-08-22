50f22b28c87ac7d7a89c82ef2a3e89f4b1f92a16
elrafael <elrafael123@gmail.com>
2026-08-22 17:40:13 +0100

    feat: refactor app layout and routing for HomePage with nested routes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,76 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive } from '@angular/router';
-import {
-  IonApp,
-  IonSplitPane,
-  IonMenu,
-  IonContent,
-  IonList,
-  IonListHeader,
-  IonNote,
-  IonMenuToggle,
-  IonItem,
-  IonIcon,
-  IonLabel,
-  IonRouterOutlet,
-  IonRouterLink,
-} from '@ionic/angular/standalone';
-import { addIcons } from 'ionicons';
-import {
-  mailOutline,
-  mailSharp,
-  paperPlaneOutline,
-  paperPlaneSharp,
-  bookmarkOutline,
-  bookmarkSharp,
-  cameraOutline,
-  cameraSharp,
-  cloudyNightOutline,
-  cloudyNightSharp,
-} from 'ionicons/icons';
+import { IonRouterOutlet, IonApp } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
-  imports: [
-    RouterLink,
-    RouterLinkActive,
-    IonApp,
-    IonSplitPane,
-    IonMenu,
-    IonContent,
-    IonList,
-    IonListHeader,
-    IonNote,
-    IonMenuToggle,
-    IonItem,
-    IonIcon,
-    IonLabel,
-    IonRouterLink,
-    IonRouterOutlet,
-  ],
+  imports: [IonRouterOutlet, IonApp],
 })
 export class AppComponent {
-  public appPages = [
-    { title: 'Camera', url: '/camera', icon: 'camera' },
-    { title: 'Weather', url: '/weather', icon: 'cloudy-night' },
-    { title: 'Inbox', url: '/folder/inbox', icon: 'mail' },
-    { title: 'Outbox', url: '/folder/outbox', icon: 'paper-plane' },
-  ];
-  public labels = ['Family', 'Friends', 'Notes'];
-  constructor() {
-    addIcons({
-      mailOutline,
-      mailSharp,
-      paperPlaneOutline,
-      paperPlaneSharp,
-      bookmarkOutline,
-      bookmarkSharp,
-      cameraOutline,
-      cameraSharp,
-      cloudyNightOutline,
-      cloudyNightSharp,
-    });
-  }
+  constructor() {}
 }
