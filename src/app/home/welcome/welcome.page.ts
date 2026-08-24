9d1afd6dc29c96975f45d18881984a04d942cea2
elrafael <elrafael123@gmail.com>
2026-08-24 09:39:47 +0100

    feat: enhance Welcome page layout and content with native feature descriptions

--- src/app/home/welcome/welcome.page.ts
+++ src/app/home/welcome/welcome.page.ts
@@ -1,20 +1,59 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import {
+  IonContent,
+  IonHeader,
+  IonTitle,
+  IonToolbar,
+  IonIcon,
+  IonText,
+  IonList,
+  IonListHeader,
+  IonLabel,
+  IonItem,
+} from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import {
+  phonePortraitOutline,
+  phonePortraitSharp,
+  cameraOutline,
+  cameraSharp,
+  locationOutline,
+  locationSharp,
+} from 'ionicons/icons';
 
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.page.html',
   styleUrls: ['./welcome.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [
+    IonContent,
+    IonHeader,
+    IonTitle,
+    IonToolbar,
+    CommonModule,
+    FormsModule,
+    IonIcon,
+    IonText,
+    IonList,
+    IonListHeader,
+    IonLabel,
+    IonItem,
+  ],
 })
 export class WelcomePage implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  constructor() {
+    addIcons({
+      phonePortraitOutline,
+      phonePortraitSharp,
+      cameraOutline,
+      cameraSharp,
+      locationOutline,
+      locationSharp,
+    });
   }
 
+  ngOnInit() {}
 }
